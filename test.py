9a8015a7513e55efd5751373f077b98e5e1c55f5
AbhiOnGit <a>
2019-09-14 00:05:21 +1000

    code abhi update

--- test.py
+++ test.py
@@ -3,3 +3,10 @@ print("Hello")
 print("Abhishjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjek")
 
 print("Abhishek")
+output_file = open("C:\Abhishek\Python\Python result\SQL_result_02.txt", "w")
+for line in range(4):
+#     output_file.write(str(line))     #line[0] points first column of output
+      output_file.write("53333")    #To remove the Parenthesis from first and last position of every record
+      output_file.write('\n')
+
+output_file.close()
